786b660cedc922aea3da061f65118d2c48eeba8f
Sergey Struk <siarhey.struk@gmail.com>
2018-06-25 07:28:40 +0300

    Added SetStatusAsync method to HiveService and HiveSectionService

--- KatlaSport.Services/HiveManagement/HiveService.cs
+++ KatlaSport.Services/HiveManagement/HiveService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using KatlaSport.DataAccess;
 using KatlaSport.DataAccess.ProductStoreHive;
 using DbHive = KatlaSport.DataAccess.ProductStoreHive.StoreHive;
 
@@ -119,7 +120,21 @@ namespace KatlaSport.Services.HiveManagement
         /// <inheritdoc/>
         public async Task SetStatusAsync(int hiveId, bool deletedStatus)
         {
-            throw new NotImplementedException();
+            var dbHives = await _context.Hives.Where(h => hiveId == h.Id).ToArrayAsync();
+
+            if (dbHives.Length == 0)
+            {
+                throw new RequestedResourceNotFoundException();
+            }
+
+            var dbHive = dbHives[0];
+            if (dbHive.IsDeleted != deletedStatus)
+            {
+                dbHive.IsDeleted = deletedStatus;
+                dbHive.LastUpdated = DateTime.UtcNow;
+                dbHive.LastUpdatedBy = _userContext.UserId;
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
